ee40a068e1498e5c26cdea0460d35281acb912d8
Ameya Bhagat <developameya@outlook.com>
2023-09-01 18:17:05 +0530

    add 'add' page and location.

---
+++ keysmith/lib/src/features/add/components/location.dart
@@ -0,0 +1,20 @@
+import 'package:beamer/beamer.dart';
+import 'package:flutter/widgets.dart';
+import 'package:keysmith/src/features/add/pages/add_page.dart';
+
+class AddLocation extends BeamLocation<BeamState> {
+  @override
+  List<BeamPage> buildPages(BuildContext context, BeamState state) {
+    final pages = [
+      if (state.uri.toString() == AddPage.routeName)
+        const BeamPage(child: AddPage())
+
+      ///if more pages are navigated to from favourite page, add them here.
+    ];
+
+    return pages;
+  }
+
+  @override
+  List<Pattern> get pathPatterns => [AddPage.routeName];
+}
