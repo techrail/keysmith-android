10b11ec86988b0b4945c560d840c2880ba49d807
Ameya Bhagat <developameya@outlook.com>
2023-09-01 18:16:48 +0530

    add favourites page and location

---
+++ keysmith/lib/src/features/favourites/components/location.dart
@@ -0,0 +1,20 @@
+import 'package:beamer/beamer.dart';
+import 'package:flutter/widgets.dart';
+import 'package:keysmith/src/features/favourites/pages/favourites_page.dart';
+
+class FavouriteLocation extends BeamLocation<BeamState> {
+  @override
+  List<BeamPage> buildPages(BuildContext context, BeamState state) {
+    final pages = [
+      if (state.uri.toString() == FavouritesPage.routeName)
+        const BeamPage(child: FavouritesPage())
+
+      ///if more pages are navigated to from favourite page, add them here.
+    ];
+
+    return pages;
+  }
+
+  @override
+  List<Pattern> get pathPatterns => [FavouritesPage.routeName];
+}
