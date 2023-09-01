a64216040bd6854bcd998a2021b77fa91798636a
Ameya Bhagat <developameya@outlook.com>
2023-09-01 18:16:33 +0530

    add home page and location.

---
+++ keysmith/lib/src/features/home/components/location.dart
@@ -0,0 +1,21 @@
+import 'package:beamer/beamer.dart';
+import 'package:flutter/widgets.dart';
+
+import 'package:keysmith/src/features/home/pages/home_page.dart';
+
+class HomeLocation extends BeamLocation<BeamState> {
+  @override
+  List<BeamPage> buildPages(BuildContext context, BeamState state) {
+    final pages = [
+      if (state.uri.toString() == HomePage.routeName)
+        const BeamPage(child: HomePage())
+
+      ///if more pages are navigated to from home page, add them here.
+    ];
+
+    return pages;
+  }
+
+  @override
+  List<Pattern> get pathPatterns => [HomePage.routeName];
+}
