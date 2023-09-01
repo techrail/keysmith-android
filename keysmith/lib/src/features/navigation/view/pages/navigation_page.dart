5293b43924e4df5414b8a660c21b72db4a63a596
Ameya Bhagat <developameya@outlook.com>
2023-09-01 18:21:56 +0530

    update navigation configuration.

--- keysmith/lib/src/features/navigation/view/pages/navigation_page.dart
+++ keysmith/lib/src/features/navigation/view/pages/navigation_page.dart
@@ -1,46 +1,24 @@
+import 'package:beamer/beamer.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:keysmith/src/features/navigation/routes/router_delegates.dart';
 import 'package:keysmith/src/features/navigation/view/components/navigation_bar_widget.dart';
 import 'package:keysmith/src/features/navigation/view/state/navigation_provider.dart';
 
 class NavigationPage extends StatelessWidget {
-  static String routeName = '/';
-
-  final pages = [
-    Container(
-      color: Colors.red,
-      alignment: Alignment.center,
-      child: const Text('Page 1'),
-    ),
-    Container(
-      color: Colors.green,
-      alignment: Alignment.center,
-      child: const Text('Page 2'),
-    ),
-    Container(
-      color: Colors.blue,
-      alignment: Alignment.center,
-      child: const Text('Page 3'),
-    ),
-    Container(
-      color: Colors.yellow,
-      alignment: Alignment.center,
-      child: const Text('Page 4'),
-    ),
-    Container(
-      color: Colors.purple,
-      alignment: Alignment.center,
-      child: const Text('Page 5'),
-    ),
-  ];
-
-  NavigationPage({super.key});
+  const NavigationPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Consumer(
-        builder: (context, ref, _) => pages[ref.watch(navigationProvider)],
+        builder: (context, ref, _) => IndexedStack(
+          index: ref.watch(navigationProvider),
+          children: List.generate(
+            routerDelegates.length,
+            (index) => Beamer(routerDelegate: routerDelegates[index]),
+          ),
+        ),
       ),
       bottomNavigationBar: const NavigationBarWidget(),
     );
