1d4c4fc3e6496a22aa3251840e8e57065af3605f
Ameya Bhagat <developameya@outlook.com>
2023-09-01 18:12:36 +0530

    remove settings controller init from main

--- keysmith/lib/main.dart
+++ keysmith/lib/main.dart
@@ -1,15 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:keysmith/src/core/common/settings/settings_controller.dart';
-import 'package:keysmith/src/core/common/settings/settings_service.dart';
+import 'package:keysmith/src/core/common/settings/init.dart';
 
 import 'src/app.dart';
 
 void main() async {
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
-  final settingsController = SettingsController(SettingsService());
-
   // Load the user's preferred theme while the splash screen is displayed.
   // This prevents a sudden theme change when the app is first displayed.
   await settingsController.loadSettings();
