7c7b8bd6a28b90ba31ce4aa27e58140e05452b43
Ameya Bhagat <developameya@outlook.com>
2023-09-22 10:19:18 +0530

    add scrollview to add password view.

--- keysmith/lib/src/features/add/presentation/views/add_password_view.dart
+++ keysmith/lib/src/features/add/presentation/views/add_password_view.dart
@@ -11,49 +11,51 @@ class AddPasswordView extends StatelessWidget {
         appBar: AppBar(),
         body: const Padding(
           padding: EdgeInsets.fromLTRB(8, 10, 8, 0),
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  Expanded(
-                      child: TextField(
-                    decoration: InputDecoration(hintText: "Title"),
-                  )),
-                  SizedBox(width: 8),
-                  InkWell(
-                    customBorder: CircleBorder(),
-                    child: CircleAvatar(
-                      maxRadius: 25,
-                      child: Icon(Icons.key_rounded),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                        child: TextField(
+                      decoration: InputDecoration(hintText: "Title"),
+                    )),
+                    SizedBox(width: 8),
+                    InkWell(
+                      customBorder: CircleBorder(),
+                      child: CircleAvatar(
+                        maxRadius: 25,
+                        child: Icon(Icons.key_rounded),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 50),
-              InputTextFieldWidget(
-                leadingIcon: Icons.person,
-                label: "Login",
-              ),
-              SizedBox(height: 50),
-              Column(
-                children: [
-                  InputTextFieldWidget(
-                    leadingIcon: Icons.key_outlined,
-                    label: "Password",
-                  ),
-                  Padding(
-                    padding: EdgeInsets.fromLTRB(45, 20, 0, 0),
-                    child: PasswordStrengthIndicatorWidget(progress: 0.5),
-                  ),
-                ],
-              ),
-              SizedBox(height: 50),
-              //TODO: validate website string
-              InputTextFieldWidget(
-                leadingIcon: Icons.language_outlined,
-                label: "Website",
-              ),
-            ],
+                  ],
+                ),
+                SizedBox(height: 50),
+                InputTextFieldWidget(
+                  leadingIcon: Icons.person,
+                  label: "Login",
+                ),
+                SizedBox(height: 50),
+                Column(
+                  children: [
+                    InputTextFieldWidget(
+                      leadingIcon: Icons.key_outlined,
+                      label: "Password",
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(45, 20, 0, 0),
+                      child: PasswordStrengthIndicatorWidget(progress: 0.5),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 50),
+                //TODO: validate website string
+                InputTextFieldWidget(
+                  leadingIcon: Icons.language_outlined,
+                  label: "Website",
+                ),
+              ],
+            ),
           ),
         ));
   }
