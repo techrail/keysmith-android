b2adee6b2a6b95da95bc4e3639310719ab1a4607
Ameya Bhagat <developameya@outlook.com>
2023-09-22 09:56:08 +0530

    add documentation to input text field widget.

--- keysmith/lib/src/features/add/presentation/widgets/input_text_field_widget.dart
+++ keysmith/lib/src/features/add/presentation/widgets/input_text_field_widget.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 
 class InputTextFieldWidget extends StatelessWidget {
   final IconData leadingIcon;
+
   final String label;
 
+  ///Creates a textfield with leading icon.
+  ///
+  ///* [leadingIcon] : The icon that will be displayed at
+  ///the start of the textfield.
+  ///* [label] : The text to be displayed when no data
+  ///in present in the textfield.
   const InputTextFieldWidget({
     super.key,
     required this.leadingIcon,
