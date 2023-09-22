bdb5bf0ff79953c85804ff290659e4fa57be0b67
Ameya Bhagat <developameya@outlook.com>
2023-09-22 09:56:49 +0530

    add password strength indicator widget to add password view.

--- keysmith/lib/src/features/add/presentation/views/add_password_view.dart
+++ keysmith/lib/src/features/add/presentation/views/add_password_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:keysmith/src/features/add/presentation/widgets/input_text_field_widget.dart';
+import 'package:keysmith/src/features/add/presentation/widgets/password_strength_indicator_widget.dart';
 
 class AddPasswordView extends StatelessWidget {
   const AddPasswordView({super.key});
@@ -19,9 +20,12 @@ class AddPasswordView extends StatelessWidget {
                     decoration: InputDecoration(hintText: "Title"),
                   )),
                   SizedBox(width: 8),
-                  CircleAvatar(
-                    maxRadius: 25,
-                    child: Icon(Icons.key_rounded),
+                  InkWell(
+                    customBorder: CircleBorder(),
+                    child: CircleAvatar(
+                      maxRadius: 25,
+                      child: Icon(Icons.key_rounded),
+                    ),
                   ),
                 ],
               ),
@@ -31,9 +35,23 @@ class AddPasswordView extends StatelessWidget {
                 label: "Login",
               ),
               SizedBox(height: 50),
+              Column(
+                children: [
+                  InputTextFieldWidget(
+                    leadingIcon: Icons.key_outlined,
+                    label: "Password",
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(45, 20, 0, 0),
+                    child: PasswordStrengthIndicatorWidget(progress: 0.5),
+                  ),
+                ],
+              ),
+              SizedBox(height: 50),
+              //TODO: validate website string
               InputTextFieldWidget(
-                leadingIcon: Icons.key_outlined,
-                label: "Password",
+                leadingIcon: Icons.language_outlined,
+                label: "Website",
               ),
             ],
           ),
