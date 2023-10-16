3e9ee652be908b792edad848cdda7c028fc70f86
Ameya Bhagat <developameya@outlook.com>
2023-10-16 17:00:20 +0530

    add id field to password entity.

--- keysmith/lib/src/features/add/data/entities/password_secret_entity.dart
+++ keysmith/lib/src/features/add/data/entities/password_secret_entity.dart
@@ -1,8 +1,11 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
 import 'package:keysmith/src/core/utils/usecase/copyable.dart';
 
 class PasswordSecretEntity extends Equatable
     implements Copyable<PasswordSecretEntity> {
+  final String id;
+
   ///Title of the secret.
   final String title;
 
@@ -24,26 +27,30 @@ class PasswordSecretEntity extends Equatable
   final Map<String, String> additionalInfo;
 
   const PasswordSecretEntity({
+    required this.id,
     required this.title,
     required this.email,
     required this.password,
-    this.website = "",
-    this.additionalInfo = const {},
+    required this.website,
+    required this.additionalInfo,
   });
 
   @override
-  List<Object> get props {
-    return [
-      title,
-      email,
-      password,
-      website,
-      additionalInfo,
-    ];
-  }
+  bool get stringify => true;
+
+  @override
+  PasswordSecretEntity copy() => PasswordSecretEntity(
+        id: id,
+        title: title,
+        email: email,
+        password: password,
+        website: website,
+        additionalInfo: additionalInfo,
+      );
 
   @override
   PasswordSecretEntity copyWith({
+    String? id,
     String? title,
     String? email,
     String? password,
@@ -51,6 +58,7 @@ class PasswordSecretEntity extends Equatable
     Map<String, String>? additionalInfo,
   }) {
     return PasswordSecretEntity(
+      id: id ?? this.id,
       title: title ?? this.title,
       email: email ?? this.email,
       password: password ?? this.password,
@@ -60,14 +68,14 @@ class PasswordSecretEntity extends Equatable
   }
 
   @override
-  bool get stringify => true;
-
-  @override
-  PasswordSecretEntity copy() => PasswordSecretEntity(
-        title: title,
-        email: email,
-        password: password,
-        website: website,
-        additionalInfo: additionalInfo,
-      );
+  List<Object> get props {
+    return [
+      id,
+      title,
+      email,
+      password,
+      website,
+      additionalInfo,
+    ];
+  }
 }
