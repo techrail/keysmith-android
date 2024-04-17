7f4f1e0386e76cf18ed721de44584a38d3fb8acb
Ameya Bhagat <developameya@outlook.com>
2024-04-17 07:38:04 +0530

    add register module to register third party libraries with DI.

---
+++ keysmith/lib/src/core/common/services/service_locator/register_module.dart
@@ -0,0 +1,9 @@
+import 'package:injectable/injectable.dart';
+
+/// A class to create and inject third party dependancies.
+///
+/// Refer [injectable] documentationfor registering third party types:
+/// https://pub.dev/packages/injectable#Registering-third-party-types
+///
+@module
+abstract class RegisterModule {}
