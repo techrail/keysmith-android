ad7661d14129edf9dff70d0381db7d4b631578ae
Ameya Bhagat <developameya@outlook.com>
2024-04-17 07:50:45 +0530

    update add password usecase implementation

--- keysmith/lib/src/features/add/domain/usecases/add_password_usecase_impl.dart
+++ keysmith/lib/src/features/add/domain/usecases/add_password_usecase_impl.dart
@@ -1,8 +1,8 @@
 import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
+import 'package:keysmith/src/core/common/entities/secrets_entity.dart';
 import 'package:keysmith/src/core/utils/error/failures.dart';
 import 'package:keysmith/src/core/utils/models/no_value.dart';
-import 'package:keysmith/src/features/add/domain/entities/password_secret_entity.dart';
 import 'package:keysmith/src/features/add/domain/repositories/add_password_repository.dart';
 import 'package:keysmith/src/features/add/domain/usecases/add_usecases.dart';
 
@@ -16,10 +16,11 @@ class AddPasswordUsecaseImpl implements AddPasswordUsecase {
   Future<Either<Failure, NoValue>> call(AddPasswordParams params) {
     return _addPasswordRepository.saveSecret(
         entity: PasswordSecretEntity(
-      id: "",
+      id: null,
       title: params.title ?? "",
       email: params.email,
       password: params.password,
+      createdAt: DateTime.now(),
       website: params.website ?? "",
       additionalInfo: const {},
     ));
