2e8812d4da563af47b5cd83dbc35537b5482224f
Ameya Bhagat <developameya@outlook.com>
2024-04-17 07:39:28 +0530

    update constants.

--- keysmith/lib/src/features/add/utils/constants/constants.dart
+++ keysmith/lib/src/features/add/utils/constants/constants.dart
@@ -1,7 +1,2 @@
-const remoteAddPasswordsourceKey = "remoteAddPasswordsource";
-const localAddPasswordsourceKey = "localAddPasswordsource";
-
-const uppercaseChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-const lowerCaseChar = 'abcdefghijklmnopqrstuvwxyz';
-const numericChar = '0123456789';
-const specialChar = '!"#\$%&\'()*+,-./:;<=>?@[\\\\]^_`{|}~ ';
+const remoteAddDatasourceKey = "remoteAddPasswordsource";
+const localAddDatasourceKey = "localAddPasswordsource";
