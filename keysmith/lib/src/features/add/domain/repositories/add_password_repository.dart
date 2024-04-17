50c47f3e84b4d2f9b7131d0878303cf93773cc5a
Ameya Bhagat <developameya@outlook.com>
2024-04-17 07:40:42 +0530

    update imports.

--- keysmith/lib/src/features/add/domain/repositories/add_password_repository.dart
+++ keysmith/lib/src/features/add/domain/repositories/add_password_repository.dart
@@ -1,7 +1,7 @@
 import 'package:dartz/dartz.dart';
+import 'package:keysmith/src/core/common/entities/secrets_entity.dart';
 import 'package:keysmith/src/core/utils/error/failures.dart';
 import 'package:keysmith/src/core/utils/models/no_value.dart';
-import 'package:keysmith/src/features/add/domain/entities/password_secret_entity.dart';
 
 ///Makes calls to datasource.
 abstract class AddPasswordRepository {
